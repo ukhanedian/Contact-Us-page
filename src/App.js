4e02c141bf18393b28a202092c99e77c5a5c8d1e
Usman Khan <ukhan.nedian@gmail.com>
2020-10-01 19:25:21 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>Contact Us</h1>
+        If you feel any queries, feel free to ask us<br/><br/>
+        Email us at <a href="mailto: promodealcompany@gmail.com">promodealcompany@gmail.com</a>
+        Or call us at our helpline <a href="callto: +923462498393">+923462498393</a>
+        Our helplines are available 24/7
+      </div>
+    );
+  }
+}
+
+export default App;
